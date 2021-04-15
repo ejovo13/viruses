401e301fc3bafe45574775cfe08761533bdd4290
Evan Voyles <ejovo13@yahoo.com>
2021-04-16 01:41:26 +0200

    Change perl command to be compatible with windows

--- +ejovo/+fn/downloadPDB.m
+++ +ejovo/+fn/downloadPDB.m
@@ -6,7 +6,7 @@ cd +v/coordinates/pdb
 %This Perl script will ONLY run if the user has the modules WWW::Mechanize
 %and IO::Uncompress::Gunzip installed. To learn how to install these
 %modules look up "install cpanm."
-output = perl('download_pdb_from_viperDB.pl', pdbid);
+output = system(strcat("perl download_pdb_from_viperDB.pl ", pdbid));
 disp(output)
 %disp(cmdout)
 %{
