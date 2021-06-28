8dd789a939e00a10eebec7b626b9631aef545ada
Evan Voyles <ejovo13@yahoo.com>
2021-06-29 00:00:22 +0200

    fix quotation error on windows

--- +ejovo/+fn/verifyMechanize.m
+++ +ejovo/+fn/verifyMechanize.m
@@ -2,13 +2,13 @@ function isInstalled = verifyMechanize
 %VERIFYMECHANIZE Check if the perl module WWW::Mechanize is installed
 %  If installed, return true. If not installed, return false.
 
-    perlCommand = "perl -e 'use WWW::Mechanize'";
+    perlCommand = 'perl -e "use WWW::Mechanize"';
     [~, output] = system(perlCommand);
-    
-    
+
+
     %isInstalled = status;
     %disp(output);
-    
+
     lenOutput = length(output);
     %disp(lenOutput);
 
